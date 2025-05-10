9f14d6593affc0f3677147b1a5bd6fa1977dde23
DESKTOP-B1BGM38\alessandro <alessandroronconi4@gmail.com>
2025-05-10 19:22:33 +0200

    risistemazione completa file, creazione connessione al db, aggiunta preliminare di tutte le classi necessarie al funzionamento dell'applicazione

---
+++ demo/src/main/java/com/example/demo/DAO/DbConnection.java
@@ -0,0 +1,7 @@
+package com.example.demo.DAO;
+
+public class DbConnection {
+    public static final String URL = "jdbc:mysql://localhost:3306/progettopozzi";
+    public static final String USER = "root";
+    public static final String PASSWORD = "";
+}
